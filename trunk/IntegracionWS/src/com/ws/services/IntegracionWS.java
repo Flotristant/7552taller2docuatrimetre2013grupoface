0aedee20a9903e909aa608fe2d9bf1031616b379
ankodelia@gmail.com <ankodelia@gmail.com@da03fd07-bea8-5617-c034-de3c9d39547d>
2013-10-26 16:22:10 +0000

    Creación de la estructura base de los WS.

---
+++ trunk/IntegracionWS/src/com/ws/services/IntegracionWS.java
@@ -0,0 +1,9 @@
+package com.ws.services;
+
+public class IntegracionWS {
+
+	public String guardarDatos() {
+		String xml = "<WS><Actividad><IdAmbito>id</IdAmbito>	</Actividad>	</WS>";
+		return xml;
+	}
+}
