66cebc7d048fd9eaac0633b2913b19ad3b75cb99
ankodelia@gmail.com <ankodelia@gmail.com@da03fd07-bea8-5617-c034-de3c9d39547d>
2013-11-01 12:29:30 +0000

    Se suben los handler y parser, interfaces

---
+++ IntegracionWS/src/com/ws/handler/UsuarioHandler.java
@@ -0,0 +1,29 @@
+package com.ws.handler;
+
+public class UsuarioHandler implements Handler {
+
+	@Override
+	public String guardarDatos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String actualizarDatos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String borrarDatos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String elegirDatos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- IntegracionWS/src/com/ws/services/IntegracionWS.java
+++ IntegracionWS/src/com/ws/services/IntegracionWS.java
@@ -1,9 +1,46 @@
 package com.ws.services;
 
-public class IntegracionWS {
+import java.io.IOException;
+import java.io.StringReader;
 
-	public String guardarDatos() {
-		String xml = "<WS><Actividad><IdAmbito>id</IdAmbito>	</Actividad>	</WS>";
-		return xml;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+public class IntegracionWS {
+		
+    public String guardarDatos() {
+        String xml = "<WS><Actividad><IdAmbito>id</IdAmbito>    </Actividad>    </WS>";
+        return xml;
+    }
+	
+	public String seleccionarDatos(String xml) {
+		
+		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder docBuilder;
+		try {
+			docBuilder = docFactory.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			
+			return "ParserConfigurationException";
+		}
+		Element doc;
+		try {
+			doc = (Element) docBuilder.parse(new InputSource(new StringReader(xml)));
+		} catch (SAXException e) {
+			return "SAXException";
+		} catch (IOException e) {
+			return "IOException";
+		} catch (Exception e) {
+			return "Exception";
+		}
+
+        NodeList nodes = doc.getElementsByTagName("Usuario");
+        return "<WS>Hola</WS>";
 	}
 }
