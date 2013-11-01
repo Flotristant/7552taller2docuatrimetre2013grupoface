2311223d5824d01989f1c1fdaaf17c5ff19af8fa
ankodelia@gmail.com <ankodelia@gmail.com@da03fd07-bea8-5617-c034-de3c9d39547d>
2013-11-01 12:29:30 +0000

    Se suben los handler y parser, interfaces

---
+++ trunk/IntegracionWS/src/com/ws/handler/Handler.java
@@ -0,0 +1,9 @@
+package com.ws.handler;
+
+public interface Handler {
+	
+	public String guardarDatos();
+	public String actualizarDatos();
+	public String borrarDatos();
+	public String elegirDatos();
+}
---
+++ trunk/IntegracionWS/src/com/ws/parsers/UsuarioParser.java
@@ -0,0 +1,25 @@
+package com.ws.parsers;
+
+import java.util.Map;
+
+public class UsuarioParser implements Parser {
+
+	@Override
+	public Boolean esJoin() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean esSelect() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, String> obtenerCampos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
