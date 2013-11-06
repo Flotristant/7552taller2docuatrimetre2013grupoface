6ceecb3c83341cbdbbf56eb58af212f9d86d261f
caviber <caviber@da03fd07-bea8-5617-c034-de3c9d39547d>
2013-11-06 04:49:57 +0000

    Subo la integracion a base de datos, no esta andando todavia. Hay que ver bien por que esta fallando y ver los errores de transacciones.

--- IntegracionWS/src/com/ws/handler/UsuarioHandler.java
+++ IntegracionWS/src/com/ws/handler/UsuarioHandler.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.w3c.dom.Document;
-
 import com.db.querys.UsuarioQueryBuilder;
 import com.ws.parsers.UsuarioParser;
 import com.ws.pojos.Usuario;
@@ -14,48 +13,40 @@ public class UsuarioHandler extends Handler {
 	
 	
 	public UsuarioHandler() {
+		super();
 		this.queryBuilder = new UsuarioQueryBuilder();
 	}
 	
 	@Override
 	public String guardarDatos(Document doc) {
-		
 		UsuarioParser parser = new UsuarioParser(doc);
+		port.beginTransaction();
 		Usuario usuario = parser.getEntidadUsuario();
-		
-		//service = new DataService();
-		//IData port = service.getDataPort();
-		//port.beginTransaccion();
-		//port.saveOrUpdate("Usuario",usuario);
-		//port.commit //if ok
-		//port.rollback()//if not
-		
-		
+		port.saveOrUpdate("ar.fiuba.redsocedu.datalayer.dtos.Usuario",usuario);
+		port.commit();//if ok
+		port.rollback();//if not
+	
 		return null;	
 	}
 
 	@Override
-	public String actualizarDatos(Document doc) {
-		
+	public String actualizarDatos(Document doc) {	
 		UsuarioParser parser = new UsuarioParser(doc);
-		//service = new DataService();
-		//IData port = service.getDataPort();
-		//port.beginTransaccion();
+		port.beginTransaction();
 		String query = this.queryBuilder.getAllById(parser.getIdUsuario());
-		Usuario usuario = null;
-		//usuario =port.query(query);
-		if(usuario == null) {
+		List usuarios = null; 
+		usuarios =port.query(query);
+		if(usuarios == null || usuarios.isEmpty() || usuarios.size() > 1) {
+			
 			//TODO: return an error message
 			return "";
 		}
-		usuario = parser.getEntidadUsuario();
+		Usuario usuario = parser.getEntidadUsuario();
 		usuario.setUsuarioId(Long.parseLong(parser.getIdUsuario()));
 		
-		
-		//port.saveOrUpdate("Usuario",usuario);
-		//port.commit //if ok
-		//port.rollback()//if not
-		
+		port.saveOrUpdate("ar.fiuba.redsocedu.datalayer.dtos.Usuario",usuario);
+		port.commit();//if ok
+		port.rollback();//if not
 		
 		return null;
 	}
@@ -63,20 +54,18 @@ public class UsuarioHandler extends Handler {
 	@Override
 	public String borrarDatos(Document doc) {
 		Map<String, String> campos = getCampos(doc);
-		//service = new DataService();
-		//IData port = service.getDataPort();
-		//port.beginTransaccion();
+		port.beginTransaction();
 		String query = this.queryBuilder.getAllById(campos.get(UsuarioTags.ID_TAG));
 		List usuarios = null; 
-		//=port.query(query);
+		port.query(query);
 		if(usuarios == null || usuarios.isEmpty() || usuarios.size() > 1) {
 			//TODO: return an error message
 			return "";
 		}
 		Usuario removingUsuario = (Usuario)usuarios.get(0);
-		//port.delete(Usuario.class, removingUsuario);
-		//port.commit //if ok
-		//port.rollback()//if not
+		port.delete("ar.fiuba.redsocedu.datalayer.dtos.Usuario",removingUsuario);
+		port.commit();//if ok
+		port.rollback();//if not
 		
 		return "";
 	}
