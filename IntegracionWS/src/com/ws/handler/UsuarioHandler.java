5b1df7a7f1b18aeafdddb873b24cb87a5a6295cb
flotristant@gmail.com <flotristant@gmail.com@da03fd07-bea8-5617-c034-de3c9d39547d>
2013-11-07 01:04:58 +0000

    agrego al parser un metodo para crear el usuario de BD

--- IntegracionWS/src/com/ws/handler/UsuarioHandler.java
+++ IntegracionWS/src/com/ws/handler/UsuarioHandler.java
@@ -4,9 +4,13 @@ import java.util.List;
 import java.util.Map;
 
 import org.w3c.dom.Document;
+
+import ar.fiuba.redsocedu.datalayer.ws.ReturnedObject;
+import ar.fiuba.redsocedu.datalayer.ws.Usuario;
+
 import com.db.querys.UsuarioQueryBuilder;
+import com.sun.xml.internal.ws.client.ClientTransportException;
 import com.ws.parsers.UsuarioParser;
-import com.ws.pojos.Usuario;
 import com.ws.tags.UsuarioTags;
 
 public class UsuarioHandler extends Handler {
@@ -21,7 +25,7 @@ public class UsuarioHandler extends Handler {
 	public String guardarDatos(Document doc) {
 		UsuarioParser parser = new UsuarioParser(doc);
 		port.beginTransaction();
-		Usuario usuario = parser.getEntidadUsuario();
+		Usuario usuario = parser.toDatabaseUser(parser.getEntidadUsuario());
 		port.saveOrUpdate("ar.fiuba.redsocedu.datalayer.dtos.Usuario",usuario);
 		port.commit();//if ok
 		port.rollback();//if not
@@ -36,37 +40,39 @@ public class UsuarioHandler extends Handler {
 		String query = this.queryBuilder.getAllById(parser.getIdUsuario());
 		List usuarios = null; 
 		usuarios =port.query(query);
-		if(usuarios == null || usuarios.isEmpty() || usuarios.size() > 1) {
-			
+		if(usuarios == null || usuarios.isEmpty() || usuarios.size() > 1) {			
 			//TODO: return an error message
 			return "";
 		}
-		Usuario usuario = parser.getEntidadUsuario();
+		Usuario usuario = parser.toDatabaseUser(parser.getEntidadUsuario());
 		usuario.setUsuarioId(Long.parseLong(parser.getIdUsuario()));
 		
 		port.saveOrUpdate("ar.fiuba.redsocedu.datalayer.dtos.Usuario",usuario);
 		port.commit();//if ok
 		port.rollback();//if not
-		
 		return null;
 	}
 
 	@Override
 	public String borrarDatos(Document doc) {
 		Map<String, String> campos = getCampos(doc);
-		port.beginTransaction();
-		String query = this.queryBuilder.getAllById(campos.get(UsuarioTags.ID_TAG));
-		List usuarios = null; 
-		port.query(query);
-		if(usuarios == null || usuarios.isEmpty() || usuarios.size() > 1) {
-			//TODO: return an error message
-			return "";
+		try {
+			port.beginTransaction();
+			String query = this.queryBuilder.getAllById(campos.get(UsuarioTags.ID_TAG));
+			List<ReturnedObject> usuarios = null; 
+			usuarios = port.query(query);
+			if(usuarios == null || usuarios.isEmpty() || usuarios.size() > 1) {
+				//TODO: return an error message
+				return "";
+			}
+			Usuario removingUsuario = (Usuario)usuarios.get(0);
+			port.delete("ar.fiuba.redsocedu.datalayer.dtos.Usuario",removingUsuario);
+			port.commit();//if ok
+			//TODO: return ok message
+		} catch(ClientTransportException e) {
+			port.rollback();//if not
+			throw e;
 		}
-		Usuario removingUsuario = (Usuario)usuarios.get(0);
-		port.delete("ar.fiuba.redsocedu.datalayer.dtos.Usuario",removingUsuario);
-		port.commit();//if ok
-		port.rollback();//if not
-		
 		return "";
 	}
 
