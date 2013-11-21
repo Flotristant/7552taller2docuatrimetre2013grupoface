abc8fd80bd63cd91bd965afd91889f4e5b685bc7
caviber <caviber@da03fd07-bea8-5617-c034-de3c9d39547d>
2013-11-21 07:10:12 +0000

    Modifico el test del cliente para poder probar el error en los servicios seleccionar datos, eliminar y actualizar.

--- ClienteIntegracionWS/src/com/ws/client/Test.java
+++ ClienteIntegracionWS/src/com/ws/client/Test.java
@@ -2,6 +2,7 @@ package com.ws.client;
 
 import java.rmi.RemoteException;
 import com.ws.services.*;
+
 import org.apache.axis2.client.Stub;
 
 public class Test {
@@ -15,23 +16,44 @@ public class Test {
 		IntegracionStub integracion = null;
 		IntegracionStub.GuardarDatos request = null;
 		IntegracionStub.GuardarDatosResponse response = null;
+		IntegracionStub.SeleccionarDatos requestSeleccionar = null;
+		IntegracionStub.SeleccionarDatosResponse responseSeleccionar = null;
+		IntegracionStub.ActualizarDatos requestActualizar = null;
+		IntegracionStub.ActualizarDatosResponse responseActualizar = null;
 		
 		try{
 			
 			integracion = new IntegracionStub();
 			request = new IntegracionStub.GuardarDatos();
 			
-			Stub stub = integracion;
+			requestSeleccionar = new IntegracionStub.SeleccionarDatos();
+			requestActualizar = new IntegracionStub.ActualizarDatos();
 			
-			stub._getServiceClient().getOptions().setTimeOutInMilliSeconds(120000);
 			
-			String xmlUser = "<?xml version=\"1.0\"?><WS><Usuario><username>usuario_prueba1</username><password>1234</password><activado>true</activado><habilitado>true</habilitado></Usuario></WS>";
+			//Stub stub = integracion;
 			
-			request.setXml(xmlUser);
+			//stub._getServiceClient().getOptions().setTimeOutInMilliSeconds(1200000);
 			
-			response = integracion.guardarDatos(request);
+			String xmlUser = "<?xml version=\"1.0\"?><WS><Usuario><username>usuarioCatalina</username><password>1234</password><activado>true</activado><habilitado>true</habilitado></Usuario></WS>";
+			//String xmlConsulta = "<?xml version=\"1.0\"?><WS><Usuario><username>testuser</username></Usuario></WS>";
+			String xmlActualizar = "<?xml version=\"1.0\"?><WS><Usuario><id>66</id><username>usuarioCatalina</username><password>123HOLA</password><activado>true</activado><habilitado>true</habilitado></Usuario></WS>";
 			
-			System.out.println(response.get_return());
+			
+			//requestSeleccionar.setXml(xmlConsulta);
+			//request.setXml(xmlConsulta);
+			requestActualizar.setXml(xmlActualizar);
+			
+			
+			//response = integracion.guardarDatos(request);
+			//responseSeleccionar = integracion.seleccionarDatos(requestSeleccionar);
+			responseActualizar = integracion.actualizarDatos(requestActualizar);
+			
+			//System.out.println("Respuesta guardar "+response.get_return());
+			System.out.println("------------------Respuesta actualizar--------------- ");
+			System.out.println("");
+			System.out.println(responseActualizar.get_return());
+			//System.out.println("Respuesta actualizar "+responseActualizar.get_return());
+			
 		
 		}catch(RemoteException excepcionDeInvocacion){
 			System.err.println(excepcionDeInvocacion.toString());
