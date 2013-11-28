7ce6ab2698f3054e87e15ce9a1a84ca9699332a8
ankodelia@gmail.com <ankodelia@gmail.com@da03fd07-bea8-5617-c034-de3c9d39547d>
2013-11-28 02:57:05 +0000

    Se agregan los últimos cambios

---
+++ trunk/IntegracionWS/src/com/ws/pojos/Foro.java
@@ -0,0 +1,20 @@
+package com.ws.pojos;
+
+public class Foro {
+	Integer id;
+	String nombre;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
