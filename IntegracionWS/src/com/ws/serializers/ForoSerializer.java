1ddb71f43d9acc7475621b39ef91d49d52ce4a67
exequiel.leite <exequiel.leite@da03fd07-bea8-5617-c034-de3c9d39547d>
2013-11-29 01:10:18 +0000

    Clases Foroserializer y SubForoSerilizer vacias para que compile el proyecto

---
+++ IntegracionWS/src/com/ws/serializers/ForoSerializer.java
@@ -0,0 +1,13 @@
+package com.ws.serializers;
+
+import com.thoughtworks.xstream.XStream;
+
+public class ForoSerializer extends Serializer{
+
+	@Override
+	protected void setAttributeMappings(XStream xstream) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
