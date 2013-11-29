38c391d8f707d2dfd6dbc4f5305b2744f6ee5ae1
flotristant@gmail.com <flotristant@gmail.com@da03fd07-bea8-5617-c034-de3c9d39547d>
2013-11-29 19:45:52 +0000

    cambios para que funcione el test de integracion de usuario

--- trunk/IntegracionWS/src/com/test/UsuarioTestIntegration.java
+++ trunk/IntegracionWS/src/com/test/UsuarioTestIntegration.java
@@ -14,144 +14,127 @@ import org.xml.sax.SAXException;
 import ar.fiuba.redsocedu.datalayer.ws.Usuario;
 
 import com.utils.NotificacionFactory;
-import com.ws.handler.UsuarioHandler;
+import com.ws.handler.UsuarioHandlerMock;
 import com.ws.parsers.UsuarioParser;
 import com.ws.serializers.NotificacionSerializer;
 import com.ws.services.IntegracionWS;
 
 public class UsuarioTestIntegration {
 
-	Usuario usuario1, usuario2;
+    Usuario usuario1, usuario2;
 
-	@Before
-	public void setUp() throws Exception {
-		IntegracionWS integracionWS = new IntegracionWS();
-		String xmlUser1 = "<?xml version=\"1.0\"?><WS><Usuario><username>usuario_prueba1</username><password>1234</password><activado>true</activado><habilitado>true</habilitado></Usuario></WS>";
+    @Before
+    public void setUp() throws Exception {
+        IntegracionWS integracionWS = new IntegracionWS();
+        integracionWS.setMockService();
+        String xmlUser1 = "<?xml version=\"1.0\"?><WS><Usuario><username>usuario_prueba1</username><password>1234</password><activado>true</activado><habilitado>true</habilitado></Usuario></WS>";
 
-		guardarDatos(xmlUser1, integracionWS);
+        guardarDatos(xmlUser1, integracionWS);
 
-		String nuevoXml1 = consultarDatos(xmlUser1, integracionWS);
-		usuario1 =obtenerUsuario(nuevoXml1, integracionWS);
-	}
-	
-	@After
-	public void cleanUp() throws Exception {
-		IntegracionWS integracionWS = new IntegracionWS();
-		String prefix = "<?xml version=\"1.0\"?><WS><Usuario><id>";
-		String suffix = "</id></Usuario></WS>";
-		String xmlUser1 = createDeleteUserXML(prefix, suffix, usuario1);
-		if(xmlUser1 != "") {
-			integracionWS.eliminarDatos(xmlUser1);	
-		}
-	}
+        String nuevoXml1 = consultarDatos(xmlUser1, integracionWS);
+        usuario1 = obtenerUsuario(nuevoXml1, integracionWS);
+    }
 
-	private Usuario obtenerUsuario(String xml, IntegracionWS integracionWS) throws SAXException, IOException, ParserConfigurationException {
-		xml = xml.replace("\n", "");
-		xml = xml.replace(" ", "");
-		UsuarioHandler handler = new UsuarioHandler();
-		UsuarioParser parser1 = new UsuarioParser();
-		parser1.inicializarDocumento(integracionWS.getXMLDocument(xml));		
-		return (Usuario) handler.toDatabaseEntity(parser1.getEntidad());
-	}
+    @After
+    public void cleanUp() throws Exception {
+        IntegracionWS integracionWS = new IntegracionWS();
+        String prefix = "<?xml version=\"1.0\"?><WS><Usuario><id>";
+        String suffix = "</id></Usuario></WS>";
+        String xmlUser1 = createDeleteUserXML(prefix, suffix, usuario1);
+        if (xmlUser1 != "") {
+            integracionWS.eliminarDatos(xmlUser1);
+        }
+    }
 
-	private void guardarDatos(String xml, IntegracionWS integracionWS) {
-		if (!integracionWS.guardarDatos(xml).equals(NotificacionSerializer.getXMLfromPojo(NotificacionFactory.Exito()))) {
-			Assert.fail("no se pudieron guardar los datos de: " + xml);
-		}
-	}
+    private Usuario obtenerUsuario(String xml, IntegracionWS integracionWS) throws SAXException, IOException, ParserConfigurationException {
+        xml = xml.replace("\n", "");
+        xml = xml.replace(" ", "");
+        UsuarioHandlerMock handler = new UsuarioHandlerMock();
+        UsuarioParser parser1 = new UsuarioParser();
+        parser1.inicializarDocumento(integracionWS.getXMLDocument(xml));
+        return (Usuario) handler.toDatabaseUser((com.ws.pojos.Usuario) parser1.getEntidad());
+    }
 
-	private String consultarDatos(String xml, IntegracionWS integracionWS) {
-		String usuarios = integracionWS.seleccionarDatos(xml);
-		if (usuarios == null || usuarios == "") {
-			Assert.fail("no se pudieron obtener los datos de: " + xml);
-		}
-		return usuarios;
-	}
+    private void guardarDatos(String xml, IntegracionWS integracionWS) {
+        if (!integracionWS.guardarDatos(xml).equals(NotificacionSerializer.getXMLfromPojo(NotificacionFactory.Exito()))) {
+            Assert.fail("no se pudieron guardar los datos de: " + xml);
+        }
+    }
 
+    private String consultarDatos(String xml, IntegracionWS integracionWS) {
+        String usuarios = integracionWS.seleccionarDatos(xml);
+        if (usuarios == null || usuarios == "") {
+            Assert.fail("no se pudieron obtener los datos de: " + xml);
+        }
+        return usuarios;
+    }
 
-	private String createDeleteUserXML(String preffix, String suffix,
-			Usuario user) {
-		if (user.getUsuarioId() != null) {
-			return preffix + user.getUsuarioId().toString() + suffix; 
-		}
-		return "";
-	}
+    private String createDeleteUserXML(String preffix, String suffix, Usuario user) {
+        if (user.getUsuarioId() != null) {
+            return preffix + user.getUsuarioId().toString() + suffix;
+        }
+        return "";
+    }
 
-	@Test
-	public void removeTest() {
-		IntegracionWS integracionWS = new IntegracionWS();
-		String xml1 = createCreateUserXML(usuario1);
-		if (!integracionWS.guardarDatos(xml1).equals(
-				NotificacionSerializer.getXMLfromPojo(NotificacionFactory
-						.Exito()))) {
-			Assert.fail("no se pudo guardar el usuario: " + xml1);
-		}
-		Assert.assertEquals(NotificacionSerializer.getXMLfromPojo(NotificacionFactory.Exito()), integracionWS.eliminarDatos(xml1));
-	}
+    @Test
+    public void removeTest() {
+        IntegracionWS integracionWS = new IntegracionWS();
+        String xml1 = createCreateUserXML(usuario1);
+        if (!integracionWS.guardarDatos(xml1).equals(NotificacionSerializer.getXMLfromPojo(NotificacionFactory.Exito()))) {
+            Assert.fail("no se pudo guardar el usuario: " + xml1);
+        }
+        Assert.assertEquals(NotificacionSerializer.getXMLfromPojo(NotificacionFactory.Exito()), integracionWS.eliminarDatos(xml1));
+    }
 
-	@Test
-	public void selectOneUserTest() {
-		try{
-		IntegracionWS integracionWS = new IntegracionWS();
-		String xml = createSelectUserXML(usuario1);
-		String salida = integracionWS.seleccionarDatos(xml);
-		System.out.println(salida);
-		}catch (Exception e){
-			e.printStackTrace();
-		}
-	}
+    @Test
+    public void selectOneUserTest() {
+        try {
+            IntegracionWS integracionWS = new IntegracionWS();
+            String xml = createSelectUserXML(usuario1);
+            String salida = integracionWS.seleccionarDatos(xml);
+            System.out.println(salida);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	private String createSelectUserXML(Usuario usuario) {
-		
-		return "<?xml version=\"1.0\"?><WS><Usuario><username>usuario_prueba1</username></Usuario></WS>";
-		
-//		return "<?xml version=\"1.0\"?><WS><Usuario><username>"
-//				+ usuario.getUsername() + "</username><password>"
-//				+ usuario.getPassword() + "</password><activado>"
-//				+ usuario.isActivado().toString() + "</activado><habilitado>"
-//				+ usuario.isHabilitado().toString()
-//				+ "</habilitado></Usuario></WS>";
-	}
+    private String createSelectUserXML(Usuario usuario) {
 
-	@Test
-	public void saveOneUserTest() {
-		IntegracionWS integracionWS = new IntegracionWS();
-		String xml = createCreateUserXML(usuario1);
-		integracionWS.guardarDatos(xml);
-	}
+        return "<?xml version=\"1.0\"?><WS><Usuario><username>usuario_prueba1</username></Usuario></WS>";
 
-	private String createCreateUserXML(Usuario usuario) {
-		return "<?xml version=\"1.0\"?><WS><Usuario><username>"
-				+ usuario.getUsername() + "</username><password>"
-				+ usuario.getPassword() + "</password><activado>"
-				+ usuario.isActivado().toString() + "</activado><habilitado>"
-				+ usuario.isHabilitado().toString()
-				+ "</habilitado></Usuario></WS>";
-	}
+        //		return "<?xml version=\"1.0\"?><WS><Usuario><username>"
+        //				+ usuario.getUsername() + "</username><password>"
+        //				+ usuario.getPassword() + "</password><activado>"
+        //				+ usuario.isActivado().toString() + "</activado><habilitado>"
+        //				+ usuario.isHabilitado().toString()
+        //				+ "</habilitado></Usuario></WS>";
+    }
 
-	@Test
-	public void updateOneUserTest() {
-		IntegracionWS integracionWS = new IntegracionWS();
-		String xml = createUpdateUserXML(usuario1);
-		integracionWS.actualizarDatos(xml);
-	}
+    @Test
+    public void saveOneUserTest() {
+        IntegracionWS integracionWS = new IntegracionWS();
+        String xml = createCreateUserXML(usuario1);
+        integracionWS.guardarDatos(xml);
+    }
 
-	private String createUpdateUserXML(Usuario usuario) {
-		if (usuario.getUsuarioId() != null) {
-			return "<?xml version=\"1.0\"?><WS><Usuario><id>"
-					+ usuario.getUsuarioId().toString() + "</id><username>"
-					+ usuario.getUsername() + "</username><password>"
-					+ usuario.getPassword() + "</password><activado>"
-					+ usuario.isActivado() + "</activado><habilitado>"
-					+ usuario.isHabilitado() + "</habilitado></Usuario></WS>";
-		} else {
-			return "<?xml version=\"1.0\"?><WS><Usuario><username>"
-					+ usuario.getUsername() + "</username><password>"
-					+ usuario.getPassword() + "</password><activado>"
-					+ usuario.isActivado() + "</activado><habilitado>"
-					+ usuario.isHabilitado() + "</habilitado></Usuario></WS>";
-		}
+    private String createCreateUserXML(Usuario usuario) {
+        return "<?xml version=\"1.0\"?><WS><Usuario><username>" + usuario.getUsername() + "</username><password>" + usuario.getPassword() + "</password><activado>" + usuario.isActivado().toString() + "</activado><habilitado>" + usuario.isHabilitado().toString() + "</habilitado></Usuario></WS>";
+    }
 
-	}
+    @Test
+    public void updateOneUserTest() {
+        IntegracionWS integracionWS = new IntegracionWS();
+        String xml = createUpdateUserXML(usuario1);
+        integracionWS.actualizarDatos(xml);
+    }
+
+    private String createUpdateUserXML(Usuario usuario) {
+        if (usuario.getUsuarioId() != null) {
+            return "<?xml version=\"1.0\"?><WS><Usuario><id>" + usuario.getUsuarioId().toString() + "</id><username>" + usuario.getUsername() + "</username><password>" + usuario.getPassword() + "</password><activado>" + usuario.isActivado() + "</activado><habilitado>" + usuario.isHabilitado() + "</habilitado></Usuario></WS>";
+        } else {
+            return "<?xml version=\"1.0\"?><WS><Usuario><username>" + usuario.getUsername() + "</username><password>" + usuario.getPassword() + "</password><activado>" + usuario.isActivado() + "</activado><habilitado>" + usuario.isHabilitado() + "</habilitado></Usuario></WS>";
+        }
+
+    }
 
 }
--- trunk/IntegracionWS/src/com/ws/handler/UsuarioHandlerMock.java
+++ trunk/IntegracionWS/src/com/ws/handler/UsuarioHandlerMock.java
@@ -1,6 +1,5 @@
 package com.ws.handler;
 
-
 import java.util.Map;
 
 import org.w3c.dom.Document;
@@ -14,126 +13,127 @@ import com.ws.parsers.UsuarioParser;
 import com.ws.serializers.NotificacionSerializer;
 import com.ws.serializers.UsuarioSerializer;
 
-
-
 public class UsuarioHandlerMock extends Handler {
-	
-	public UsuarioHandlerMock() {
-		super("ar.fiuba.redsocedu.datalayer.dtos.Usuario", new UsuarioParser(), new UsuarioSerializer());
-		this.queryBuilder = new UsuarioQueryBuilder();
-	}
-	
-	@Override
-	public String guardarDatos(Document doc) {
-		
-		MockCache.guardarUsuario(doc);
-		this.parser.inicializarDocumento(doc);
-		
-//		UsuarioParser parser = new UsuarioParser(doc);
-//		try{
-//			port.beginTransaction();
-//			Usuario usuario = this.toDatabaseUser(parser.getEntidadUsuario());
-//			port.saveOrUpdate("ar.fiuba.redsocedu.datalayer.dtos.Usuario",usuario);
-//			port.commit();
-//		}
-//		catch(ClientTransportException e) {
-//			port.rollback();
-//			return NotificacionSerializer.getXMLfromPojo(NotificacionFactory.Error());
-//		}
-	
-		return NotificacionSerializer.getXMLfromPojo(NotificacionFactory.Exito());	
-		
-		
-	}
 
-	@Override
-	public String actualizarDatos(Document doc) {
-		MockCache.guardarUsuario(doc);
-		return NotificacionSerializer.getXMLfromPojo(NotificacionFactory.Exito());
-	}
+    public UsuarioHandlerMock() {
+        //super("ar.fiuba.redsocedu.datalayer.dtos.Usuario", new UsuarioParser(), new UsuarioSerializer());
+        super(new UsuarioParser(), new UsuarioSerializer());
+        this.queryBuilder = new UsuarioQueryBuilder();
+    }
 
-	@Override
-	public String borrarDatos(Document doc) {
-		
-		if(MockCache.borrarUsuario(doc)) return NotificacionSerializer.getXMLfromPojo(NotificacionFactory.Exito());
-		else return NotificacionSerializer.getXMLfromPojo(NotificacionFactory.sinResultados());
-	}
+    @Override
+    public String guardarDatos(Document doc) {
 
-	@Override
-	public String seleccionarDatos(Document doc) {
-		
-		String salida = MockCache.seleccionarUsuario(doc);
-		
-		if (salida == null) return NotificacionSerializer.getXMLfromPojo(NotificacionFactory.sinResultados());
-		else return salida;
-		
-//		
-//		Usuario miusuario = new Usuario();
-//		
-//		miusuario.setActivado(true);
-//		miusuario.setApellido("Perez");
-//		miusuario.setNombre("Alfonso");
-//		miusuario.setEmail("ap@fiuba.edu.ar");
-//		miusuario.setPadron("1234");
-//		miusuario.setPassword("123456");
-//		miusuario.setHabilitado(true);
-//		
-//		XMLGregorianCalendar fecha = new XMLGregorianCalendarImpl();
-//		fecha.setYear(1986);
-//		fecha.setMonth(5);
-//		fecha.setDay(5);
-//		
-//		miusuario.setFechaNac(fecha);
-//		miusuario.setUsername("Pepe");
-//		miusuario.setUsuarioId(12L);
-//		
-//		return UsuarioSerializer.getXMLfromPojo(miusuario);
-	}
-	
-//	@Override
-//	public String seleccionarDatos(Document doc) {
-//		UsuarioParser parser = new UsuarioParser(doc);
-//		Usuario miusuario  = parser.getEntidadUsuario();
-//
-//		miusuario.setEmail("ap@fiuba.edu.ar");
-//		miusuario.setPadron("1234");
-//		
-//		XMLGregorianCalendar fecha = new XMLGregorianCalendarImpl();
-//		fecha.setYear(1986);
-//		fecha.setMonth(5);
-//		fecha.setDay(5);
-//		
-//		miusuario.setFechaNac(fecha);
-//		miusuario.setUsuarioId(12L);
-//		
-//		return UsuarioSerializer.getXMLfromPojo(miusuario);
-//	}
-	
-	
-	protected Map<String, String> getCampos(Document doc) {
-		this.parser.inicializarDocumento(doc);
-		return parser.obtenerCampos();
-	}
-	
-	public Usuario toDatabaseUser(com.ws.pojos.Usuario usuario) {
-		ar.fiuba.redsocedu.datalayer.ws.Usuario user = new ar.fiuba.redsocedu.datalayer.ws.Usuario();
-		user.setNombre(usuario.getNombre());
-		user.setApellido(usuario.getApellido());
-		user.setPadron(usuario.getPadron());
-		//user.setFechaNac(usuario.getFechaNacimiento());
-		user.setActivado(usuario.getActivado());
-		user.setHabilitado(usuario.getHabilitado());
-		user.setEmail(usuario.getEmail());
-		user.setPassword(usuario.getPassword());
-		user.setUsername(usuario.getUsername());
-		user.setUsuarioId(usuario.getId());
-		return user;
-	}
+        MockCache.guardarUsuario(doc);
+        this.parser.inicializarDocumento(doc);
 
-	@Override
-	protected Object toDatabaseEntity(Object object) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        //		UsuarioParser parser = new UsuarioParser(doc);
+        //		try{
+        //			port.beginTransaction();
+        //			Usuario usuario = this.toDatabaseUser(parser.getEntidadUsuario());
+        //			port.saveOrUpdate("ar.fiuba.redsocedu.datalayer.dtos.Usuario",usuario);
+        //			port.commit();
+        //		}
+        //		catch(ClientTransportException e) {
+        //			port.rollback();
+        //			return NotificacionSerializer.getXMLfromPojo(NotificacionFactory.Error());
+        //		}
+
+        return NotificacionSerializer.getXMLfromPojo(NotificacionFactory.Exito());
+
+    }
+
+    @Override
+    public String actualizarDatos(Document doc) {
+        MockCache.guardarUsuario(doc);
+        return NotificacionSerializer.getXMLfromPojo(NotificacionFactory.Exito());
+    }
+
+    @Override
+    public String borrarDatos(Document doc) {
+
+        if (MockCache.borrarUsuario(doc))
+            return NotificacionSerializer.getXMLfromPojo(NotificacionFactory.Exito());
+        else
+            return NotificacionSerializer.getXMLfromPojo(NotificacionFactory.sinResultados());
+    }
+
+    @Override
+    public String seleccionarDatos(Document doc) {
+
+        String salida = MockCache.seleccionarUsuario(doc);
+
+        if (salida == null)
+            return NotificacionSerializer.getXMLfromPojo(NotificacionFactory.sinResultados());
+        else
+            return salida;
+
+        //		
+        //		Usuario miusuario = new Usuario();
+        //		
+        //		miusuario.setActivado(true);
+        //		miusuario.setApellido("Perez");
+        //		miusuario.setNombre("Alfonso");
+        //		miusuario.setEmail("ap@fiuba.edu.ar");
+        //		miusuario.setPadron("1234");
+        //		miusuario.setPassword("123456");
+        //		miusuario.setHabilitado(true);
+        //		
+        //		XMLGregorianCalendar fecha = new XMLGregorianCalendarImpl();
+        //		fecha.setYear(1986);
+        //		fecha.setMonth(5);
+        //		fecha.setDay(5);
+        //		
+        //		miusuario.setFechaNac(fecha);
+        //		miusuario.setUsername("Pepe");
+        //		miusuario.setUsuarioId(12L);
+        //		
+        //		return UsuarioSerializer.getXMLfromPojo(miusuario);
+    }
+
+    //	@Override
+    //	public String seleccionarDatos(Document doc) {
+    //		UsuarioParser parser = new UsuarioParser(doc);
+    //		Usuario miusuario  = parser.getEntidadUsuario();
+    //
+    //		miusuario.setEmail("ap@fiuba.edu.ar");
+    //		miusuario.setPadron("1234");
+    //		
+    //		XMLGregorianCalendar fecha = new XMLGregorianCalendarImpl();
+    //		fecha.setYear(1986);
+    //		fecha.setMonth(5);
+    //		fecha.setDay(5);
+    //		
+    //		miusuario.setFechaNac(fecha);
+    //		miusuario.setUsuarioId(12L);
+    //		
+    //		return UsuarioSerializer.getXMLfromPojo(miusuario);
+    //	}
+
+    protected Map<String, String> getCampos(Document doc) {
+        this.parser.inicializarDocumento(doc);
+        return parser.obtenerCampos();
+    }
+
+    public Usuario toDatabaseUser(com.ws.pojos.Usuario usuario) {
+        ar.fiuba.redsocedu.datalayer.ws.Usuario user = new ar.fiuba.redsocedu.datalayer.ws.Usuario();
+        user.setNombre(usuario.getNombre());
+        user.setApellido(usuario.getApellido());
+        user.setPadron(usuario.getPadron());
+        //user.setFechaNac(usuario.getFechaNacimiento());
+        user.setActivado(usuario.getActivado());
+        user.setHabilitado(usuario.getHabilitado());
+        user.setEmail(usuario.getEmail());
+        user.setPassword(usuario.getPassword());
+        user.setUsername(usuario.getUsername());
+        user.setUsuarioId(usuario.getId());
+        return user;
+    }
+
+    @Override
+    protected Object toDatabaseEntity(Object object) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
