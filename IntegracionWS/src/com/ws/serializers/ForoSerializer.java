cf895fe4c5d7f1e43a539411c2362a5ae19bef61
caviber <caviber@da03fd07-bea8-5617-c034-de3c9d39547d>
2013-12-02 17:28:51 +0000

    Subo actualizacion de ForoSerializer y SubforoSerializer

--- IntegracionWS/src/com/ws/serializers/ForoSerializer.java
+++ IntegracionWS/src/com/ws/serializers/ForoSerializer.java
@@ -1,12 +1,17 @@
 package com.ws.serializers;
 
 import com.thoughtworks.xstream.XStream;
+import com.ws.pojos.Foro;
+import com.ws.tags.ForoTags;
 
 public class ForoSerializer extends Serializer{
 
 	@Override
 	protected void setAttributeMappings(XStream xstream) {
-		// TODO Auto-generated method stub
+		xstream.alias(ForoTags.CLASS_TAG, Foro.class);
+		xstream.aliasField(ForoTags.ID_TAG, Foro.class, "idForo");
+		xstream.aliasField(ForoTags.NOMBRE_TAG, Foro.class, "nombre");
+		xstream.aliasField(ForoTags.ID_AMBITO_TAG, Foro.class, "idAmbito");
 		
 	}
 
--- IntegracionWS/src/com/ws/serializers/SubforoSerializer.java
+++ IntegracionWS/src/com/ws/serializers/SubforoSerializer.java
@@ -1,12 +1,23 @@
 package com.ws.serializers;
 
 import com.thoughtworks.xstream.XStream;
+import com.ws.pojos.Subforo;
+import com.ws.tags.SubforoTags;
 
 public class SubforoSerializer extends Serializer {
 
 	@Override
 	protected void setAttributeMappings(XStream xstream) {
-		// TODO Auto-generated method stub
+		xstream.alias(SubforoTags.CLASS_TAG, Subforo.class);
+		xstream.aliasField(SubforoTags.ID_TAG, Subforo.class, "id");
+		xstream.aliasField(SubforoTags.NOMBRE_TAG, Subforo.class, "nombre");
+		xstream.aliasField(SubforoTags.ID_FORO_TAG, Subforo.class, "idForo");
+		xstream.aliasField(SubforoTags.ID_SUBFOROPADRE_TAG, Subforo.class, "idSubforoPadre");
+		xstream.aliasField(SubforoTags.ID_SECCION_TAG, Subforo.class, "idSeccion");
+		
+		
+		
+		
 		
 	}
 
