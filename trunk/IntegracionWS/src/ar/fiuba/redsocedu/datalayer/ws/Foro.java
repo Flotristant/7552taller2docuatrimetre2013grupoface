943cbdcd5bda7f2df27ed810f7f42d95831206b0
exequiel.leite <exequiel.leite@da03fd07-bea8-5617-c034-de3c9d39547d>
2013-12-06 02:23:40 +0000

    Nueva clase ambito y otros pojos actualizados en DB

--- trunk/IntegracionWS/src/ar/fiuba/redsocedu/datalayer/ws/Foro.java
+++ trunk/IntegracionWS/src/ar/fiuba/redsocedu/datalayer/ws/Foro.java
@@ -19,6 +19,8 @@ import javax.xml.bind.annotation.XmlType;
  *   &lt;complexContent>
  *     &lt;extension base="{http://ws.datalayer.redsocedu.fiuba.ar/}returnedObject">
  *       &lt;sequence>
+ *         &lt;element name="ambito" type="{http://ws.datalayer.redsocedu.fiuba.ar/}ambito" minOccurs="0"/>
+ *         &lt;element name="ambitoId" type="{http://www.w3.org/2001/XMLSchema}long" minOccurs="0"/>
  *         &lt;element name="foroId" type="{http://www.w3.org/2001/XMLSchema}long" minOccurs="0"/>
  *         &lt;element name="nombre" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
  *         &lt;element name="secciones" type="{http://ws.datalayer.redsocedu.fiuba.ar/}seccion" maxOccurs="unbounded" minOccurs="0"/>
@@ -32,6 +34,8 @@ import javax.xml.bind.annotation.XmlType;
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "foro", propOrder = {
+    "ambito",
+    "ambitoId",
     "foroId",
     "nombre",
     "secciones"
@@ -40,10 +44,60 @@ public class Foro
     extends ReturnedObject
 {
 
+    protected Ambito ambito;
+    protected Long ambitoId;
     protected Long foroId;
     protected String nombre;
     @XmlElement(nillable = true)
     protected List<Seccion> secciones;
+
+    /**
+     * Gets the value of the ambito property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Ambito }
+     *     
+     */
+    public Ambito getAmbito() {
+        return ambito;
+    }
+
+    /**
+     * Sets the value of the ambito property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Ambito }
+     *     
+     */
+    public void setAmbito(Ambito value) {
+        this.ambito = value;
+    }
+
+    /**
+     * Gets the value of the ambitoId property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Long }
+     *     
+     */
+    public Long getAmbitoId() {
+        return ambitoId;
+    }
+
+    /**
+     * Sets the value of the ambitoId property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Long }
+     *     
+     */
+    public void setAmbitoId(Long value) {
+        this.ambitoId = value;
+    }
 
     /**
      * Gets the value of the foroId property.
