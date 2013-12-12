765d87f52b9f7ad67ac7ae8e1bbcaf84f8b48bed
flotristant@gmail.com <flotristant@gmail.com@da03fd07-bea8-5617-c034-de3c9d39547d>
2013-12-12 03:20:02 +0000

    Cambios para adaptar parsers a los joins.

--- trunk/IntegracionWS/src/com/ws/parsers/ChatParser.java
+++ trunk/IntegracionWS/src/com/ws/parsers/ChatParser.java
@@ -1,15 +1,55 @@
 package com.ws.parsers;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.w3c.dom.NodeList;
+
+import com.thoughtworks.xstream.XStream;
+import com.ws.handler.Handler;
 import com.ws.pojos.Chat;
+import com.ws.pojos.MensajeChat;
+import com.ws.pojos.MiembroChat;
 import com.ws.tags.ChatTags;
 
 public class ChatParser extends Parser {
-
+	
+	Map<String, String> relaciones;
 	
 	public ChatParser() {
 		super(ChatTags.CLASS_TAG);
+		relaciones = new HashMap<String, String>();
+		relaciones.put(MensajeChatParser.class.toString(), "mensajesChat");
+		relaciones.put(MiembroChatParser.class.toString(), "miembrosChat");
 	}
 	
+	@Override
+	public Map<String, String> getJoinFields() {
+		String joinXML = this.campos.get(Parser.JOIN_TAG);
+		Map<String, String> joinFields = new HashMap<String, String>();
+		Parser parser = getJoinParser(joinXML);
+		if(parser == null)
+			return joinFields;
+		String nombreRelacion = relaciones.get(parser.getClass().toString());
+		joinXML = joinXML.replace("<join>", "");
+		joinXML = joinXML.replace("</join>", "");
+		parser.inicializarCampos(joinXML);
+		Map<String, String> camposRelacion = parser.getCampos();
+		for(String campo : camposRelacion.keySet()) {
+			joinFields.put(nombreRelacion+"."+campo,camposRelacion.get(campo));
+		}
+		return joinFields;		
+	}
+	
+	private Parser getJoinParser(String joinXML) {
+		Parser parser = getParserFromJoinXML(joinXML);
+		if((parser instanceof MensajeChatParser)||(parser instanceof MiembroChatParser)) {
+			return parser;
+		}
+		return null;
+	}
+
+
 	@Override
 	public Long getId() {
 		return Long.parseLong(this.campos.get(ChatTags.ID_TAG));
--- trunk/IntegracionWS/src/com/ws/tags/ChatTags.java
+++ trunk/IntegracionWS/src/com/ws/tags/ChatTags.java
@@ -5,5 +5,7 @@ public class ChatTags {
 	public static String CLASS_TAG = "Chat";
 	public static String ID_TAG = "idChat";
 	public static String ID_AMBITO_TAG = "idAmbito";
+	public static String MENSAJES_CHAT = "mensajesChat";
+	public static String MIEMBROS_CHAT = "miembrosChat";
 
 }
