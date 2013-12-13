7357bc082ca86b9157eb65508122af95003274d5
caviber@gmail.com <caviber@gmail.com@da03fd07-bea8-5617-c034-de3c9d39547d>
2013-12-13 18:59:43 +0000

    Actividad (Nota y Grupo) - Me falto comitear los query builder, ahi van.

---
+++ IntegracionWS/src/com/db/querys/GrupoQueryBuilder.java
@@ -0,0 +1,11 @@
+package com.db.querys;
+
+import com.ws.tags.GrupoTags;
+
+public class GrupoQueryBuilder extends QueryBuilder {
+
+	public GrupoQueryBuilder() {
+		super("ar.fiuba.redsocedu.datalayer.dtos.Grupo", GrupoTags.ID_TAG);
+	}
+
+}
