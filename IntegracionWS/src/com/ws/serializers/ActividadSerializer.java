8093945886b7115ae11d2564054bdef5e00e1a07
flotristant@gmail.com <flotristant@gmail.com@da03fd07-bea8-5617-c034-de3c9d39547d>
2013-12-13 16:14:13 +0000

    Completo actividadSerializer, me habia quedado vacia

--- IntegracionWS/src/com/ws/serializers/ActividadSerializer.java
+++ IntegracionWS/src/com/ws/serializers/ActividadSerializer.java
@@ -1,13 +1,38 @@
 package com.ws.serializers;
 
-import com.thoughtworks.xstream.XStream;
+import ar.fiuba.redsocedu.datalayer.ws.Actividad;
 
+import com.thoughtworks.xstream.XStream;
+import com.ws.tags.ActividadTags;
+
+/**
+ * 
+ * @author $Author$
+ * @version $Revision$
+ */
 public class ActividadSerializer extends Serializer {
 
-	@Override
-	protected void setAttributeMappings(XStream xstream) {
-		// TODO Auto-generated method stub
+    @Override
+    protected void setAttributeMappings(XStream xstream) {
+        //El nombre de la clase (atributo raiz) sera
+        xstream.alias(ActividadTags.CLASS_TAG, Actividad.class);
+        //Mapeos de los nombres de atributo
+        xstream.aliasField(ActividadTags.ID_TAG, Actividad.class, "id");
+        xstream.aliasField(ActividadTags.ACTIVIDAD_ID_TAG, Actividad.class, "actividadId");
+        xstream.aliasField(ActividadTags.ACTIVIDAD_SUPERIOR_ID_TAG, Actividad.class, "actividadSuperiorId");
+        xstream.aliasField(ActividadTags.AMBITO_SUPERIOR_ID_TAG, Actividad.class, "ambitoSuperiorId");
+        xstream.aliasField(ActividadTags.DESCRIPCION_TAG, Actividad.class, "descripcion");
+        xstream.aliasField(ActividadTags.FECHA_FIN_TAG, Actividad.class, "fechaFin");
+        xstream.aliasField(ActividadTags.FECHA_INICIO_TAG, Actividad.class, "fechaInicio");
+        xstream.aliasField(ActividadTags.GRUPOS_TAG, Actividad.class, "grupos");
+        xstream.aliasField(ActividadTags.GRUPOSEXCLUSIVO_TAG, Actividad.class, "gruposExclusivo");
+        xstream.aliasField(ActividadTags.NOMBRE_TAG, Actividad.class, "nombre");
+        xstream.aliasField(ActividadTags.NOTAS_TAG, Actividad.class, "notas");
+        xstream.aliasField(ActividadTags.TIPO_TAG, Actividad.class, "tipo");
+        xstream.aliasField(ActividadTags.TIPO_ESCALA_TAG, Actividad.class, "tipoEscala");
 
-	}
+        xstream.aliasPackage("", "ar.fiuba.redsocedu.datalayer.ws");
+
+    }
 
 }
