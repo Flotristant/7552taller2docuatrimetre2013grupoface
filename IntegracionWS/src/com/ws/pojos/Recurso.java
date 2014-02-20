a8844a35ba25ce611ea83c6fa40ec50eb5abfa97
caviber <caviber@da03fd07-bea8-5617-c034-de3c9d39547d>
2014-02-20 11:32:55 +0000

    [Materiales] Issue 13, 14.

--- IntegracionWS/src/com/ws/pojos/Recurso.java
+++ IntegracionWS/src/com/ws/pojos/Recurso.java
@@ -35,11 +35,9 @@ public class Recurso extends Pojo{
 	public Object getDatabaseEntity() {
 		ar.fiuba.redsocedu.datalayer.ws.Recurso recursoDB = new ar.fiuba.redsocedu.datalayer.ws.Recurso();
 		recursoDB.setAmbitoId(this.getAmbitoId());
-		recursoDB.setRecursoId(this.getId());
 		recursoDB.setId(this.getId());
 		recursoDB.setDescripcion(this.getDescripcion());
 		recursoDB.setTipo(this.getTipo());
-		
 		return recursoDB;
 	}
 	
